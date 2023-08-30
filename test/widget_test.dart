6dc2240792153542d3d147b6e10c445b179a0399
ferdinaand <ekpo546@gmail.com>
2023-08-30 10:44:40 +0100

    update

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:travely/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const TravelyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
