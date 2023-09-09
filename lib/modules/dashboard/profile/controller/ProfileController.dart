fa8ff1685b8f2765a99ad591b6970558a49c078f
ferdinaand <ekpo546@gmail.com>
2023-09-09 17:52:53 +0100

    update

--- lib/modules/dashboard/profile/controller/ProfileController.dart
+++ lib/modules/dashboard/profile/controller/ProfileController.dart
@@ -5,6 +5,7 @@ import 'package:get/get_rx/src/rx_types/rx_types.dart';
 import 'package:intl/intl.dart';
 import '../../../../controller/base.controller.dart';
 import '../../../../routing/app_pages.dart';
+import '../../../authentication/user_signin/controller/user.signinController.dart';
 
 class ProfileController extends BaseController {
   var isLoading = false.obs;
@@ -12,5 +13,12 @@ class ProfileController extends BaseController {
   @override
   void onInit() {
     super.onInit();
+  }
+
+  final UserSigninController userSigninController =
+      Get.find<UserSigninController>();
+
+  void signout() {
+    userSigninController.signOutUser();
   }
 }
