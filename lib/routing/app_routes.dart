9824156a8551ea122084d4d509803f07faf13905
ferdinaand <ekpo546@gmail.com>
2023-09-09 17:53:25 +0100

    update

--- lib/routing/app_routes.dart
+++ lib/routing/app_routes.dart
@@ -12,6 +12,7 @@ abstract class Routes {
   static const phoneScreen = '/app/phoneScreen';
   static const verifyScreen = '/app/verifyScreen';
   static const verifiedScreen = '/app/verifiedScreen';
+  static const createUser = '/app/createUser';
   // static const login = '/app/login';
   // static const register = '/app/register';
   // static const home = '/app/home';
