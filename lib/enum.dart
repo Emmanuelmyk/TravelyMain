8e2e89a072d1fb9e4262f5e00ecc7b59a8ec6f12
ferdinaand <ekpo546@gmail.com>
2023-09-10 14:52:36 +0100

    update

---
+++ lib/enum.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+enum ServiceType { HeavyDutyHaulage, DomesticGoodsHaulage, TankerHaulage }
+
+enum TruckType {
+  Tanker,
+  twoxFlatBed,
+  tipper,
+  open,
+  covered,
+  flatbed,
+  box,
+}
